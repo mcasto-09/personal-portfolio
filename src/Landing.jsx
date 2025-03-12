33045771457b134f156c5247838e67a2669e27dc
Mary Castorani <mgcastorani@gmail.com>
2025-03-12 13:00:14 -0600

    tailwind configs added

--- src/Landing.jsx
+++ src/Landing.jsx
@@ -3,18 +3,29 @@ import "./Landing.css";
 
 function App() {
   const [count, setCount] = useState(0);
+  const TypingEffect = () => {
+    // return (
+    //   <div className="flex min-h-screen items-center justify-center bg-gradient-to-tr to-blue-700 from-indigo-900 p-10">
+    //     <div className="w-max">
+    //     </div>
+    //   </div>
+    // );
+  };
 
   return (
     <div className="App">
       <div>
-        <a href="https://vitejs.dev" target="_blank">
+        <a href="https://vitejs.dev" target="_blank"></a>
+        <a href="https://reactjs.org" target="_blank">
+          {" "}
         </a>
-        <a href="https://reactjs.org" target="_blank"> </a>
       </div>
-      <h1>Hello</h1>
-      <h2>I'm Mary,</h2>
-      <h3>I'll be your next favorite developer</h3>
+      <div className="animate-typing overflow-hidden whitespace-nowrap border-r-4 border-r-white pr-5 text-5xl text-white font-bold">
+        <h1>Hello</h1>
+        <h2>I'm Mary,</h2>
+        <h3>I'll be your next favorite developer</h3>
       </div>
+    </div>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,35 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{html,js,jsx,ts,tsx}'], // Update paths as per your project
+  content: ["./src/**/*.{html,js,jsx,ts,tsx}"], // Update paths as per your project
+
   theme: {
     extend: {
-      fontFamily: {
+       fontFamily: {
         spaceGrotesk: ['"Space Grotesk"', 'sans-serif'], // Add "Space Grotesk"
+      keyframes: {
+        typing: {
+          "0%": {
+            width: "0%",
+            visibility: "hidden",
+          },
+          "100%": {
+            width: "100%",
+          },
+        },
+        blink: {
+          "50%": {
+            borderColor: "transparent",
+          },
+          "100%": {
+            borderColor: "white",
+          },
+        },
+      },
+      animation: {
+        typing: "typing 2s steps(20) infinite alternate, blink .7s infinite",
       },
     },
   },
   plugins: [],
-};
-
+  }
+}
